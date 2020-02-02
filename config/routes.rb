80d77e395a41b7c4cae51ef255ae98312e41ff26
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-02-03 01:23:03 +0900

    投稿機能とビュー調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'maps#index'
+  root to: 'toppage#index'
   resources :maps, only: [:index]
   get '/map_request', to: 'maps#map', as: 'map_request'
 
