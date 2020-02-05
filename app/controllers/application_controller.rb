278069fae6599a4e9c0fd9043c67e0df38db9755
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-02-05 16:58:45 +0900

    投稿機能と一覧表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def create_google_map_marker
+    @hash = Gmaps4rails.build_markers(@posts) do |post, marker|
+    marker.lat post.latitude
+    marker.lng post.longitude
+    marker.infowindow post.title
+    # marker.infowindow render_to_string(partial: 'shard/infowindow', locals: { post: post })
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,12 +1,13 @@
 class MapsController < ApplicationController
   def index
+    @posts = Post.all
+    create_google_map_marker
   end
 
   def map
     results = Geocoder.search(params[:address])
     @latlng = results.first.coordinates
     # これでmap.js.erbで、経度緯度情報が入った@latlngを使える。
-  
     respond_to do |format|
       format.js
     end
