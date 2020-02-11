a313810651a076a0771b2dd3612cd9eec46eea33
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-02-11 20:36:58 +0900

    S3機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.new(post_params)
     if @post.save
       redirect_to maps_path
     else
